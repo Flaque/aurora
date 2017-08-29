6a177f251d45d04660dad2d29f4196cea0b1646b
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-08-29 09:32:55 -0700

    remove lerna

--- packages/aurora-electron/src/renderer/index.js
+++ packages/aurora-electron/src/renderer/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Core from "@aurora/core";
+import Core from "aurora-core";
 
 // Loads in "Core" onto the electron page.
 ReactDOM.render(<Core />, document.getElementById("root"));
--- packages/aurora-electron/webpack.config.renderer.js
+++ packages/aurora-electron/webpack.config.renderer.js
@@ -23,6 +23,10 @@ module.exports = {
     ]
   },
   resolve: {
-    modules: [path.join(__dirname, "src"), "node_modules"]
+    modules: [
+      path.join(__dirname, "src"),
+      path.join(__dirname, "..", "../"),
+      "node_modules"
+    ]
   }
 };
